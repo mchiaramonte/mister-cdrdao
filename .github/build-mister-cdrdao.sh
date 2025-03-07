1c6a904592f292acabf2539e3a1afb2478817145
David Gillies <dave.gillies@gmail.com>
2025-03-07 11:55:55 -0800

    Refactor build-mister-cdrdao.sh
    * Get artifact versions from env vars
    * Download artifacts instead of git checkouts
    * DRY up getredumpdata and move it into build-mister-cdrdao

--- .github/build-mister-cdrdao.sh
+++ .github/build-mister-cdrdao.sh
@@ -1,45 +1,33 @@
 #!/bin/bash
+set -euo pipefail
 
 STARTDIR=$PWD
 
 # Get and unpack the ARM compiler tools for x64
-wget -q -c https://developer.arm.com/-/media/Files/downloads/gnu-a/10.2-2020.11/binrel/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf.tar.xz
-sudo tar xf gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf.tar.xz -C /opt
-rm gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf.tar.xz
+wget -q -c https://developer.arm.com/-/media/Files/downloads/gnu-a/${GCC_VERSION}/binrel/gcc-arm-${GCC_VERSION}-x86_64-arm-none-linux-gnueabihf.tar.xz
+sudo tar xf gcc-arm-${GCC_VERSION}-x86_64-arm-none-linux-gnueabihf.tar.xz -C /opt
+rm gcc-arm-${GCC_VERSION}-x86_64-arm-none-linux-gnueabihf.tar.xz
 
 # Setup the PATH to reference the newly unpacked tools.
 # IMPORTANT: This export is required for configure to pickup the ARM tools
-# IMPORTANT: so the path must be exported like this. The update to .bashrc
-# IMPORTANT: is purely for convenience later if you decide to run the tools
-echo 'export PATH=/opt/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin:$PATH' >> ~/.bashrc
-export PATH=/opt/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin:$PATH
+# IMPORTANT: so the path must be exported like this.
+export PATH=/opt/gcc-arm-${GCC_VERSION}-x86_64-arm-none-linux-gnueabihf/bin:$PATH
 
 # Update symbolic links to reference the ARM libraries needed for the build
-sudo ln -s /opt/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/arm-none-linux-gnueabihf/libc/lib/ld-linux-armhf.so.3 /lib/ld-linux-armhf.so.3
-sudo ln -s /opt/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/arm-none-linux-gnueabihf/libc/lib/libc.so.6 /lib/libc.so.6
-sudo ln -s /opt/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/arm-none-linux-gnueabihf/libc/lib/libpthread.so.0 /lib/libpthread.so.0
+sudo ln -s /opt/gcc-arm-${GCC_VERSION}-x86_64-arm-none-linux-gnueabihf/arm-none-linux-gnueabihf/libc/lib/ld-linux-armhf.so.3 /lib/ld-linux-armhf.so.3
+sudo ln -s /opt/gcc-arm-${GCC_VERSION}-x86_64-arm-none-linux-gnueabihf/arm-none-linux-gnueabihf/libc/lib/libc.so.6 /lib/libc.so.6
+sudo ln -s /opt/gcc-arm-${GCC_VERSION}-x86_64-arm-none-linux-gnueabihf/arm-none-linux-gnueabihf/libc/lib/libpthread.so.0 /lib/libpthread.so.0
 
-# Get latest version of CDRDAO
-if [ ! -d "cdrdao-repo" ]; then
-    git clone https://github.com/cdrdao/cdrdao.git cdrdao-repo
-fi
+wget -q https://github.com/putnam/binmerge/releases/download/${BINMERGE_VERSION}/binmerge-${BINMERGE_VERSION}.zip
+unzip binmerge-${BINMERGE_VERSION}.zip
+mv -v binmerge-${BINMERGE_VERSION}/binmerge $STARTDIR/Scripts/.config/mister-cdrdao
+rm -rf binmerge-${BINMERGE_VERSION} ${BINMERGE_VERSION}.zip
 
-# Get the latest version of binmerge
-if [ ! -d "binmerge-repo" ]; then
-    git clone https://github.com/putnam/binmerge.git binmerge-repo
-fi
-
-# Copy the latest version of binmerge for splitting the BIN/CUE files
-cd binmerge-repo
-git checkout main
-git pull
-cd $STARTDIR
-mv -v ./binmerge-repo/binmerge $STARTDIR/Scripts/.config/mister-cdrdao
+wget -q https://github.com/cdrdao/cdrdao/releases/download/${CDRDAO_RELEASE}/cdrdao-${CDRDAO_VERSION}.tar.bz2
+tar xf cdrdao-${CDRDAO_VERSION}.tar.bz2
 
 # Go into the cdrdao-repo directory and configure the build to run for ARM Linux
-cd cdrdao-repo
-git checkout master
-git pull
+cd cdrdao-${CDRDAO_VERSION}
 ./autogen.sh
 ./configure --host=arm-none-linux-gnueabihf
 make all
@@ -50,8 +38,11 @@ mv -v \
   $STARTDIR/Scripts/.config/mister-cdrdao
 
 cd $STARTDIR
-bash .github/getredumpdata.sh
-cd $STARTDIR
-mv -v *.dat $STARTDIR/Scripts/.config/mister-cdrdao
-rm -rf binmerge-repo cdrdao-repo
-find Scripts
+rm -rf cdrdao-${CDRDAO_VERSION} cdrdao-${CDRDAO_VERSION}.tar.bz2
+
+for system in PSX Saturn MegaCD TGFX16-CD NeoGeo-CD; do
+  echo "==> Downloading ${system} dat file..."
+  wget -q http://redump.org/datfile/psx/ -O ${system}.zip
+  unzip -p ${system}.zip > $STARTDIR/Scripts/.config/mister-cdrdao/${system}.dat
+  rm ${system}.zip
+done
